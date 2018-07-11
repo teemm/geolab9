72376913d3eab996d5d901bd037277abe15d140c
TeeMM <temo@geolab.edu.ge>
2018-07-11 12:50:05 +0400

    ბოლო ვერსია

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ $("#burger").on('click', function(){
 	$('#menu').toggleClass('active');
 });
 
+
+$("#search-button").on("click", function(e){
+	// console.log(e);
+	// e.preventDefault();
+	$(".search-block").addClass("active");
+	$("body").addClass("hidden");
+});
+
+$("#search-close").on("click", function(){
+	$(".search-block").removeClass("active");
+	$("body").removeClass("hidden");
+});
+
+// function test(test1, test2){
+	// console.log(test1);
+// }
+
+
+// test(1,2);
